03a9d66a0d7928fb1798ce9aa7af38c777fec777
James Hubert <jwhubert91@gmail.com>
2023-03-11 14:50:45 -0500

    more home styling

---
+++ components/ticker.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import ReactTicker from "react-ticker"
+
+function Ticker() {
+  return (
+    // <ReactTicker>
+    //   {({ index }) => (
+    //     <>
+    //       <h1>This is the Headline of element #{index}!</h1>
+    //     </>
+    //   )}
+    // </ReactTicker>
+    <div></div>
+  )
+}
+
+export default Ticker
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Layout from "@/components/layout"
+import Ticker from "@/components/ticker"
 import Image from "next/image"
 // import { Inter } from "next/font/google"
 // import styles from "@/styles/Home.module.css"
@@ -8,14 +9,14 @@ export default function Home() {
   return (
     <Layout className="layout__container">
       <Panel height="full">
-        <h1 className="text-5xl md:text-[150px] md:leading-[170px] font-bold mb-2">
+        <h1 className="text-6xl md:text-[90px] md:leading-[100px] lg:text-[110px] lg:leading-[110px] xl:text-[150px] xl:leading-[170px] font-bold mb-5">
           Local shopping in New York City
         </h1>
-        <h2 className="text-3xl font-medium mb-5">
+        <h2 className="text-2xl md:text-3xl font-medium mb-5">
           Find what you need in local stores. Order online and pick up same day.
         </h2>
         <div className="mx-auto w-[fit-content] italic mb-10">Coming soon!</div>
-        <div className="max-w-[600px] mx-auto border-4 border-black p-5">
+        <div className="max-w-[600px] mx-auto border-4 border-black p-5 mb-[100px] lg:mb-0">
           <p className="mb-3 text-lg">
             Merchants request to join the beta and begin listing your products
             to increase pick up orders today 📈
@@ -27,6 +28,7 @@ export default function Home() {
           </div>
         </div>
       </Panel>
+      {/* <Ticker /> */}
     </Layout>
   )
 }
