7f04ec96163e19719be9d72b004a02bd0640dd41
James Hubert <jwhubert91@gmail.com>
2023-04-08 15:20:31 -0400

    change seo page name

--- components/seoHead.tsx
+++ components/seoHead.tsx
@@ -19,7 +19,7 @@ function SEOHead({ headData }: Props) {
   } = headData
   return (
     <Head>
-      <title>{`${pageTitle} | ListIt.one`}</title>
+      <title>{`${pageTitle} | ListIt Local Shopping`}</title>
       <meta name="description" content={pageDescription} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/listit-favicon.ico" />
